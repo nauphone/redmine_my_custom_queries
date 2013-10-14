a7f21e028e1832590dcb13634a3352fff66bbaed
Alexei Margasov <amargasov@naumen.ru>
2013-10-14 16:56:26 +0600

    update plugin

---
+++ init.rb
@@ -0,0 +1,14 @@
+require 'redmine'
+
+ActionDispatch::Callbacks.to_prepare  do
+  MyController.send(:helper, :queries)
+end
+
+Redmine::Plugin.register :redmine_my_custom_queries do
+  name 'My Custom Queries'
+  author '---'
+  description 'Adds custom queries onto My Page screen'
+  version '0.0.1'
+  author_url ''
+end
+
